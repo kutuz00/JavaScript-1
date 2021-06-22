c306c2a7bddc762705bd80d15186fe737da7234a
Kostyantyn <kutuz00@gmail.com>
2021-06-21 23:21:35 -0700

    chess table

--- script.js
+++ script.js
@@ -1,125 +1,134 @@
-// 1. Написать функцию, преобразующую число в объект. Передавая на вход число от 0 до 999,
-// мы должны получить на выходе объект, в котором в соответствующих свойствах описаны единицы, десятки и сотни. 
-// Например, для числа 245 мы должны получить следующий объект: {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}.
-// Если число превышает 999, необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
 
-// var n = prompt("enter any number from 0 to 999");
-// var numObject = {};
-
-// function transformation(string) {
-//     if (string.length - 1 == 2) {
-//         numObject.hundreds = +string[0];
-//         numObject.dozens = +string[1];
-//         numObject.units = +string[2];
-//         return numObject;
-//     }
-//     else if (string.length - 1 == 1) {
-//         numObject.hundreds = 0;
-//         numObject.dozens = +string[0];
-//         numObject.units = +string[1];
-//         return numObject;
-//     }
-//     else if (string.length - 1 == 0) {
-//         numObject.hundreds = 0;
-//         numObject.dozens = 0;
-//         numObject.units = +string[0];
-//         return numObject;
-//     }
-//     console.log("Number exceed max");
-//     return numObject;
-// }
-// transformation(n);
+var count = 0;
+var labels = document.querySelectorAll('.row-number');
+var labelsRight = document.querySelectorAll('.row-number-right');
+var topLabels = document.querySelector(".labels-top");
+var bottomLabels = document.querySelector(".labels-bottom");
+var row = document.querySelector('.wrap');
 
 
+for (var i = 9; i <= 16; i++) {
+    drawLabels(topLabels, ".labels-top", "top-cell");
+}
+for (var i = 9; i <= 16; i++) {
+    drawLabels(bottomLabels, ".labels-bottom", "bottom-cell");
+}
+for (var i = 1; i <= 8; i++) {
+    drawLabels(labels, ".row-number", "cell");
+}
+for (var i = 1; i <= 8; i++) {
+    drawLabels(labelsRight, ".row-number-right", "rotate");
+}
+for (var i = 1; i <= 8; i++) {
+    drawRows(count++);
+}
+function drawLabels(name, selector, css) {
+    name = document.createElement('div');
+
+    switch (i) {
+        case 9:
+            name.innerHTML = "<h4>A</h4>";
+            break;
+        case 10:
+            name.innerHTML = "<h4>B</h4>";
+            break;
+        case 11:
+            name.innerHTML = "<h4>C</h4>";
+            break;
+        case 12:
+            name.innerHTML = "<h4>D</h4>";
+            break;
+        case 13:
+            name.innerHTML = "<h4>E</h4>";
+            break;
+        case 14:
+            name.innerHTML = "<h4>F</h4>";
+            break;
+        case 15:
+            name.innerHTML = "<h4>G</h4>";
+            break;
+        case 16:
+            name.innerHTML = "<h4>H</h4>";
+            break;
+        default:
+            name.innerHTML = "<h4>" + (i) + "</h4>";
+            break;
+    }
+    name.classList.add(css);
+    document.querySelector(selector).append(name);
+
+}
+function drawRows(count) {
+    for (var i = 1; i <= 8; i++) {
+        row = document.createElement('div');
+        row.innerHTML = "<h3></h3>";
+        if (count % 2 == 0) {
+            row.classList.add("block-black");
+        }
+        else row.classList.add("block");
+        document.querySelector(".wrap").append(row);
+        count++;
+    }
+}
+
+var item = document.querySelectorAll("h3");
 
 
-// 2. Для игры Квест, реализованной на уроке, добавить возможность вывода хода номер n (номер задается пользователем)
+for (var i = 0; i <= item.length - 1; i++) {
+    switch (i) {
 
+        case 0:
+        case 7:
+            item[i].classList.add("black");
+        case 56:
+        case 63:
+            item[i].innerHTML = "&#9814";
+            break;
+        case 1:
+        case 6:
+            item[i].classList.add("black");
+        case 57:
+        case 62:
+            item[i].innerHTML = "&#9816";
+            break;
+        case 2:
+        case 5:
+            item[i].classList.add("black");
+        case 58:
+        case 61:
+            item[i].innerHTML = "&#9815";
+            break;
+        case 3:
+            item[i].classList.add("black");
+        case 59:
+            item[i].innerHTML = "&#9812";
+            break;
+        case 4:
+            item[i].classList.add("black");
+        case 60:
+            item[i].innerHTML = "&#9813";
+            break;
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            item[i].classList.add("black");
+        case 48:
+        case 49:
+        case 50:
+        case 51:
+        case 52:
+        case 53:
+        case 54:
+        case 55:
+            item[i].innerHTML = "&#9817";
+            break;
+        default: break;
+    }
 
+}
 
-// 3. *На базе игры, созданной на уроке, реализовать игру «Кто хочет стать миллионером?»
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100
-// var num = 2, step = 2;
-// while (num < 100) {
-//     if (num == 2) {
-//         console.log(num);
-//         ++num;
-//     }
-//     else if (num % step === 0 && num > step) {
-//         step = 2;
-//         ++num;
-//     }
-//     else if (num % step != 0) {
-//         step++;
-//     }
-//     else if (num === step) {
-//         console.log(num);
-//         ++num;
-//         step = 2;
-//     }
-
-// }
-// // 2. С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
-// // 3. Товары в корзине хранятся в массиве. Задачи:
-// // a) Организовать такой массив для хранения товаров в корзине;
-// // b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
-// var goods = [{
-//     title: "Item 1",
-//     price: 125,
-//     count: 5
-// },
-// {
-//     title: "Item 2",
-//     price: 155,
-//     count: 5
-// },
-// {
-//     title: "Item 3",
-//     price: 100,
-//     count: 5
-// }
-// ];
-
-// function countBasketPrice() {
-//     var subtotal = 0;
-//     for (var item of goods) {
-//         subtotal += item.price;
-//         console.log(item.title + " cost " + item.price * item.count);
-//     }
-
-// }
-// countBasketPrice(goods);
-
-
-
-// // 4.*Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так: 
-// // for(…){// здесь пусто}
-// for (var i = 0; i <= 9; console.log(i++));
-
-
-
-// // 4) *Нарисовать  пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
-// var element = "x"
-// for (var i = 0; i < 20; i++) {
-//     console.log(element);
-//     element += "x"
-// }
-
-// // x
-// // xx
-// // xxx
-// // xxxx
-// // xxxxx
