32f8d08a02cf1ef9d9096c1919b591affc9569fe
Kostyantyn <kutuz00@gmail.com>
2021-06-26 02:25:00 -0700

    Working on basket

--- script.js
+++ script.js
@@ -1,134 +1,66 @@
-
-var count = 0;
-var labels = document.querySelectorAll('.row-number');
-var labelsRight = document.querySelectorAll('.row-number-right');
-var topLabels = document.querySelector(".labels-top");
-var bottomLabels = document.querySelector(".labels-bottom");
-var row = document.querySelector('.wrap');
-
-
-for (var i = 9; i <= 16; i++) {
-    drawLabels(topLabels, ".labels-top", "top-cell");
-}
-for (var i = 9; i <= 16; i++) {
-    drawLabels(bottomLabels, ".labels-bottom", "bottom-cell");
-}
-for (var i = 1; i <= 8; i++) {
-    drawLabels(labels, ".row-number", "cell");
-}
-for (var i = 1; i <= 8; i++) {
-    drawLabels(labelsRight, ".row-number-right", "rotate");
-}
-for (var i = 1; i <= 8; i++) {
-    drawRows(count++);
-}
-function drawLabels(name, selector, css) {
-    name = document.createElement('div');
-
-    switch (i) {
-        case 9:
-            name.innerHTML = "<h4>A</h4>";
-            break;
-        case 10:
-            name.innerHTML = "<h4>B</h4>";
-            break;
-        case 11:
-            name.innerHTML = "<h4>C</h4>";
-            break;
-        case 12:
-            name.innerHTML = "<h4>D</h4>";
-            break;
-        case 13:
-            name.innerHTML = "<h4>E</h4>";
-            break;
-        case 14:
-            name.innerHTML = "<h4>F</h4>";
-            break;
-        case 15:
-            name.innerHTML = "<h4>G</h4>";
-            break;
-        case 16:
-            name.innerHTML = "<h4>H</h4>";
-            break;
-        default:
-            name.innerHTML = "<h4>" + (i) + "</h4>";
-            break;
-    }
-    name.classList.add(css);
-    document.querySelector(selector).append(name);
-
-}
-function drawRows(count) {
-    for (var i = 1; i <= 8; i++) {
-        row = document.createElement('div');
-        row.innerHTML = "<h3></h3>";
-        if (count % 2 == 0) {
-            row.classList.add("block-black");
+var items = document.querySelectorAll(".item");
+var basketOfGoods = [];
+var button = document.createElement("button");
+var buttons;
+var item = document.createElement("h1");
+var img = document.createElement("img");
+var price = document.createElement("span");
+var goods = [{
+    title: "item",
+    price: 200,
+    quantity: 10,
+    productImage: "img/image"
+},
+{
+    title: "item",
+    price: 330,
+    quantity: 8,
+    productImage: "img/image"
+},
+{
+    title: "item",
+    price: 150,
+    quantity: 5,
+    productImage: "img/image"
+}];
+showCatalog();
+buttons = document.getElementsByTagName('button');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function (e) {
+        var basket = {};
+        basket.itemName = e.target.parentElement.firstChild.innerText;
+        basket.itemPrice = parseInt(e.target.previousElementSibling.innerText);
+        basket.itemCount = 1;
+        console.log(basketOfGoods.length);
+        if (basketOfGoods.length === 0) {
+            basketOfGoods.push(basket);
         }
-        else row.classList.add("block");
-        document.querySelector(".wrap").append(row);
-        count++;
-    }
-}
+        else
+            for (var j = 0; j < basketOfGoods.length; j++) {
 
-var item = document.querySelectorAll("h3");
+                if (basketOfGoods[j].itemName === basket.itemName && basketOfGoods[j].itemPrice === basket.itemPrice) {
+                    basketOfGoods[j].itemCount = basketOfGoods[j].itemCount + basket.itemCount;
+                    break;
+                }
+                else
+                    basketOfGoods.push(basket);
 
 
-for (var i = 0; i <= item.length - 1; i++) {
-    switch (i) {
+            }
+        console.log(basket);
+        console.log(basketOfGoods);
 
-        case 0:
-        case 7:
-            item[i].classList.add("black");
-        case 56:
-        case 63:
-            item[i].innerHTML = "&#9814";
-            break;
-        case 1:
-        case 6:
-            item[i].classList.add("black");
-        case 57:
-        case 62:
-            item[i].innerHTML = "&#9816";
-            break;
-        case 2:
-        case 5:
-            item[i].classList.add("black");
-        case 58:
-        case 61:
-            item[i].innerHTML = "&#9815";
-            break;
-        case 3:
-            item[i].classList.add("black");
-        case 59:
-            item[i].innerHTML = "&#9812";
-            break;
-        case 4:
-            item[i].classList.add("black");
-        case 60:
-            item[i].innerHTML = "&#9813";
-            break;
-        case 8:
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-            item[i].classList.add("black");
-        case 48:
-        case 49:
-        case 50:
-        case 51:
-        case 52:
-        case 53:
-        case 54:
-        case 55:
-            item[i].innerHTML = "&#9817";
-            break;
-        default: break;
     }
 
 }
+function showCatalog() {
+    for (var i = 0; i < goods.length; i++) {
+        item.innerText = goods[i].title + (i + 1);
+        img.src = goods[i].productImage + i + ".jpg";
+        price.innerText = goods[i].price;
+        button.innerText = "Add to cart " + (i + 1);
+        items[i].innerHTML = item.outerHTML + img.outerHTML + price.outerHTML + button.outerHTML;
+        document.querySelector(".items").append(items[i]);
 
+    }
+}
